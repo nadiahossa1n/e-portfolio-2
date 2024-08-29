5c022bdc33b469ea20734982c5d2f22b5acec9c4
Nadia <nh894@nyu.edu>
2024-08-29 08:47:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+About Me
+Hi there! I'm Nadia, a passionate web developer with a strong focus on building responsive, user-friendly websites. My expertise lies in frontend technologies including HTML, modern CSS frameworks, JavaScript, and React. I take pride in transforming design ideas into functional, dynamic web applications that provide exceptional user experiences.
+
+What You’ll Find Here
+Projects: A showcase of my recent work, including detailed descriptions and links to live demos or code repositories.
+Skills: A summary of my technical skills, including frontend technologies, tools, and best practices I use in my development process.
+Contact: Information on how to get in touch with me for potential collaborations, questions, or opportunities.
---
+++ index.js
@@ -0,0 +1,55 @@
+let contrastToggle = false;
+let isModalOpen = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_43i3vga",
+      "template_hylkrpc",
+      event.target,
+      "U825GTDPigap3q1zL"
+    ).then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at nh894@nyu.edu."
+      );
+    });
+}
+
+function toggleModal() {
+    isModalOpen = !isModalOpen;
+    if (isModalOpen) {
+        return document.body.classList.remove("modal--open");
+    }
+    document.body.classList += " modal--open";
+}
