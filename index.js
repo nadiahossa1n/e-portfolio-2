555beba732d1d04ae6c6cba3c6bcd8994a4f0e0f
Nadia <nh894@nyu.edu>
2024-10-07 16:33:37 -0400

    links, projects, and color scheme updated

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 let contrastToggle = false;
 let isModalOpen = false;
-const scaleFactor = 1 / 20;
-
-function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape");
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
-
-    for (let i = 0; i < shapes.length; ++i) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
-    }
-}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
